c09193ab387e979a2cae8645761051bcddbfc3bf
Unknown <raekwongerold@hotmail.com>
2019-04-17 11:30:24 +0200

    helloWorld huiswerk

---
+++ Week 1/Hello World/js/helloWorld.js
@@ -0,0 +1,12 @@
+/*jslint browser: true, devel: true, eqeq: true, plusplus: true, sloppy: true, vars: true, white: true*/
+
+/*eslint-env browser*/
+
+/*eslint 'no-console': 0*/
+
+console.log("Hello World");
+document.querySelector('h1').textContent = "Hello World!";
+
+var naam = 'Raekwon Gerold';
+    naam = 'Waarom moet dit nou?';
+document.querySelector('h1').textContent = naam;

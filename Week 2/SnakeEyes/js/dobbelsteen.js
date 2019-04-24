bf20604c2438753ffacfb66e70fc7424ce57b434
Unknown <raekwongerold@hotmail.com>
2019-04-24 14:25:23 +0200

    Finishing snake eyes

--- Week 2/SnakeEyes/js/dobbelsteen.js
+++ Week 2/SnakeEyes/js/dobbelsteen.js
@@ -7,12 +7,14 @@
 var rolgetal;
 var imgArray = ["one.png", "two.png", "three.png", "four.png", "five.png", "six.png"];
 var dobbelsteenImg;
+var dobbelgeluid;
+dobbelgeluid = new Audio("sfx/shake_dice.mp3");
 
 function werp(){
-
+    document.querySelector("img").classList.add("throw");
     rolgetal = Math.floor(Math.random() * 6);
     dobbelsteenImg = imgArray[rolgetal];
-
+    dobbelgeluid.play();
     document.querySelector("img").src = "images/" + dobbelsteenImg;
 }
 
